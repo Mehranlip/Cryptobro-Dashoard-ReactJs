ce5930e120f79a384c9f5dd7ea6387ced287dbe4
Mehran <contact@mehranlip.ir>
2023-08-24 13:35:13 +0330

    end side bar and fix sm responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,99 @@
 import React from 'react'
+import { useState } from 'react';
 import sidebar_data from '../data/sidebar-data'
 import { Link } from 'react-router-dom'
 
 function Sidebar() {
+    const [focusedIndex, setFocusedIndex] = useState(null);
+
+    const handleLinkFocus = (index) => {
+        setFocusedIndex(index);
+    };
+
+
+
 
     return (
-        <div className='col-2 px-5 py-2 sidebar_body vh-100'>
-            {/* logo */}
-            <div className='mb-4'>
-                <img src='../../public/logo/Logo.svg' />
-            </div>
-            {/* end logo */}
-            {
-                sidebar_data.map((item) => (
-                    <div key={item.title}>
-                        <div className='row justify-content-center align-items-center text-cneter'>
-                            <Link to={item.link} className='col text-decoration-none mb-4 defult_itme-sidebar px-3 py-2'>
-                                <img className='mx-2' src={item.image} />
-                                <span>{item.title}</span>
+        <>
+            {/* nav in sm */}
+            <div className='d-sm-none d-block p-0'>
+                <div class="collapse " id="navbarToggleExternalContent">
+                    <div class="sidebar_body p-4 text-center">
+                        {
+                            sidebar_data.map((item, index) => (
+                                <div key={item.title}>
+                                    <div className='row justify-content-center align-items-center text-justify'>
+                                        <Link
+                                            onFocus={() => handleLinkFocus(index)}
+                                            onBlur={() => setFocusedIndex(null)}
+                                            to={item.link} className='col text-decoration-none mb-4 defult_itme-sidebar px-3 py-2'>
+                                            <img className='mx-2' src={focusedIndex === index ? item.image_light : item.image} />
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                        {/* logout logo */}
+                        <div className='row justify-content-center align-items-center text-justify fixed-button mt-5'>
+                            <Link
+                                className='col text-decoration-none mb-4  logout-sidebar px-3 py-2 mt-5'>
+                                <img className='mx-2' src='../../public/white-icon/Styleguide/Logout.svg' />
+                                <span>Logout</span>
                             </Link>
                         </div>
+                        {/* end logout logo */}
                     </div>
+                </div>
+                <nav class="navbar navbar-dark sidebar_body">
+                    <div class="container-fluid">
+                        {/* logo */}
+                        <div>
+                            <img src='../../public/logo/Logo.svg' />
+                        </div>
+                        {/* end logo */}
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+            {/* end nav in sm */}
 
-                ))
-            }
-        </div>
+            {/* sidebar in md ... */}
+            <div className='d-none d-md-inline col-12 col-md-2 px-5 py-2 sidebar_body vh-100'>
+                {/* logo */}
+                <div className='mb-4'>
+                    <img src='../../public/logo/Logo.svg' />
+                </div>
+                {/* end logo */}
+                {
+                    sidebar_data.map((item, index) => (
+                        <div key={item.title}>
+                            <div className='row justify-content-center align-items-center text-justify'>
+                                <Link
+                                    onFocus={() => handleLinkFocus(index)}
+                                    onBlur={() => setFocusedIndex(null)}
+                                    to={item.link} className='col text-decoration-none mb-4 defult_itme-sidebar px-3 py-2'>
+                                    <img className='mx-2' src={focusedIndex === index ? item.image_light : item.image} />
+                                    <span>{item.title}</span>
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+                {/* logout logo */}
+                <div className='row justify-content-center align-items-center text-justify fixed-button mt-5'>
+                    <Link
+                        className='col text-decoration-none mb-4  logout-sidebar px-3 py-2 mt-5'>
+                        <img className='mx-2' src='../../public/white-icon/Styleguide/Logout.svg' />
+                        <span>Logout</span>
+                    </Link>
+                </div>
+                {/* end logout logo */}
+            </div>
+            {/* end sidebar in md */}
+        </>
     )
 }
 
--- src/data/sidebar-data.js
+++ src/data/sidebar-data.js
@@ -2,35 +2,41 @@ const sidebar_data = [
     {
         title: "Dashboard",
         image: "../../public/icons/Home.svg",
+        image_light: "../../public/white-icon/Styleguide/Home.svg",
         link: "/"
     },
     {
         title: "Exchange",
         image: "../../public/icons/Swap.svg",
+        image_light: "../../public/white-icon/Styleguide/Swap.svg",
         link: "/dashboard/exchange"
 
     },
     {
         title: "Wallet",
         image: "../../public/icons/Iconly/Bold/Wallet.svg",
+        image_light: "../../public/white-icon/Styleguide/Iconly/Bold/Wallet.svg",
         link: "/dashboard/wallet"
 
     },
     {
         title: "Statistics",
         image: "../../public/icons/Activity.svg",
+        image_light: "../../public/white-icon/Styleguide/Activity.svg",
         link: "/dashboard/statistics"
 
     },
     {
         title: "Profile",
         image: "../../public/icons/Profile.svg",
+        image_light: "../../public/white-icon/Styleguide/Profile.svg",
         link: "/dashboard/profile"
 
     },
     {
         title: "Settings",
         image: "../../public/icons/Setting.svg",
+        image_light: "../../public/white-icon/Styleguide/Setting.svg",
         link: "/dashboard/settings"
 
     },
